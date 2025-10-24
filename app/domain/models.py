58e66dcba0ce224c751c584a188aa04755dd6699
Libing Chen <libing.chen@gmail.com>
2025-10-24 20:14:37 +0800

    feat: add BaseDictModel and convert model to dict

--- app/domain/models.py
+++ app/domain/models.py
@@ -5,17 +5,31 @@ from sqlalchemy import DateTime, Integer, Text, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 import datetime
 
+
 class Base(DeclarativeBase):
     pass
 
+
 db = SQLAlchemy(model_class=Base)
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+
+class BaseDictModel(db.Model):
+    __abstract__ = True
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Hero(BaseDictModel):
+    __tablename__ = "hero"
 
     name: Mapped[str] = mapped_column(Text)
     power: Mapped[str] = mapped_column(Text)
     age: Mapped[int] = mapped_column(Integer)
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    created_at: Mapped[Optional[datetime.datetime]] = mapped_column(DateTime, server_default=text('current_timestamp'))
-    updated_at: Mapped[Optional[datetime.datetime]] = mapped_column(DateTime, server_default=text('current_timestamp'))
+    created_at: Mapped[Optional[datetime.datetime]] = mapped_column(
+        DateTime, server_default=text("current_timestamp")
+    )
+    updated_at: Mapped[Optional[datetime.datetime]] = mapped_column(
+        DateTime, server_default=text("current_timestamp")
+    )
