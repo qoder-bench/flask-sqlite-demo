09671e7af17d6d34ceed2937abd33eb5323b9c25
Libing Chen <libing.chen@gmail.com>
2025-10-24 20:45:04 +0800

    test: add test_user_query

--- tests/test_db.py
+++ tests/test_db.py
@@ -9,3 +9,9 @@ def test_user_model():
     users = Hero.query.all()
     for user in users:
         print(user.to_dict())
+
+
+@pytest.mark.usefixtures("app_ctx")
+def test_user_query():
+    user = Hero.query.where(Hero.name == "Batman").first()
+    print(user.to_dict())
